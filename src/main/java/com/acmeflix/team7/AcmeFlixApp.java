ea59ee43746fcc538d1728d52e458b2b8f04600f
Petros Dhespollari <peterdsp@icloud.com>
2022-08-01 13:46:44 +0300

    Update AcmeFlixApp

--- src/main/java/com/acmeflix/team7/AcmeFlixApp.java
+++ src/main/java/com/acmeflix/team7/AcmeFlixApp.java
@@ -1,4 +1,11 @@
 package com.acmeflix.team7;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AcmeFlixApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeFlixApp.class, args);
+    }
 }
