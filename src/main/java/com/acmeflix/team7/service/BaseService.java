9a232c34406b407d31d4615910904ad2ed869d50
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-03 17:06:09 +0300

    BaseService creation and implementation.

--- src/main/java/com/acmeflix/team7/service/BaseService.java
+++ src/main/java/com/acmeflix/team7/service/BaseService.java
@@ -1,4 +1,25 @@
 package com.acmeflix.team7.service;
 
-public class BaseService {
+import com.acmeflix.team7.domain.BaseModel;
+
+import java.util.List;
+
+public interface BaseService<T extends BaseModel> {
+    T create(final T entity);
+
+    List<T> createAll(final T... entities);
+
+    List<T> createAll(final List<T> entities);
+
+    void update(T entity);
+
+    void delete(T entity);
+
+    void deleteById(Long id);
+
+    boolean exists(T entity);
+
+    T get(Long id);
+
+    List<T> findAll();
 }
