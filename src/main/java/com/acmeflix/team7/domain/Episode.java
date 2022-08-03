ebc3d994db8a45d220b8b86b52211d347c7c24c2
Petros Dhespollari <peterdsp@icloud.com>
2022-08-03 17:07:03 +0300

    Create Episode

---
+++ src/main/java/com/acmeflix/team7/domain/Episode.java
@@ -0,0 +1,9 @@
+package com.acmeflix.team7.domain;
+
+import java.math.BigDecimal;
+
+public class Episode extends BaseModel{
+    private String title;
+    private Date date;
+    private BigDecimal duration;
+}
