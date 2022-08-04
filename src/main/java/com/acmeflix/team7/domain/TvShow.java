e3b5250e54ac9732ccc2f513be9b10fa1081e1c6
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 14:41:21 +0300

    Update TvShow

--- src/main/java/com/acmeflix/team7/domain/TvShow.java
+++ src/main/java/com/acmeflix/team7/domain/TvShow.java
@@ -1,5 +1,15 @@
 package com.acmeflix.team7.domain;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
 public class TvShow extends ContentCatalog{
     private List<Season> seasons;
 }
