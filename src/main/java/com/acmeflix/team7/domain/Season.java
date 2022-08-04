6a71b2e1346ba68a1a31947e5f0a5151b7086255
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 14:40:49 +0300

    Update Season

--- src/main/java/com/acmeflix/team7/domain/Season.java
+++ src/main/java/com/acmeflix/team7/domain/Season.java
@@ -1,5 +1,15 @@
 package com.acmeflix.team7.domain;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
 public class Season extends BaseModel{
     private String title, year;
     private List<Episode> episodes;
