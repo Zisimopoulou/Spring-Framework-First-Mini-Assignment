e9f5738f1586ab39b0654d9ffd303384736016b5
d.paliogiannis <oldjohn0000@gmail.com>
2022-08-04 12:41:47 +0300

    dimitris

--- src/main/java/com/acmeflix/team7/domain/enums/Country.java
+++ src/main/java/com/acmeflix/team7/domain/enums/Country.java
@@ -1,11 +1,31 @@
 package com.acmeflix.team7.domain.enums;
 
+
 import lombok.Getter;
 
 @Getter
+
 public enum Country {
-    Austria,
-	German,
-	France,
-    Greece;
+    Austria("austria"),
+	German("german"),
+	France("france"),
+    Greece("greece");
+
+    private String country;
+    Country(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
 }
+
+
+
+
+
+
+
+
