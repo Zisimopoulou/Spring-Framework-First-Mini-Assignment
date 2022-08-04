18a744699adf7d84e7f20ae768a2a38ece24a632
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 14:43:39 +0300

    Create TvShowServiceImpl

---
+++ src/main/java/com/acmeflix/team7/service/TvShowServiceImpl.java
@@ -0,0 +1,7 @@
+package com.acmeflix.team7.service;
+
+import com.acmeflix.team7.domain.UserAccount;
+
+public interface TvShowServiceImpl extends BaseServiceImpl<UserAccount> implements TvShowService{
+
+}
