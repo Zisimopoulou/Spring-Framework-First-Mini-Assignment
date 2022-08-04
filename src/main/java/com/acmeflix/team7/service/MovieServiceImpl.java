b7d5706e9d5b05885fcdecc752fefb43c9ff8f0a
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 14:39:41 +0300

    Update MovieServiceImpl

--- src/main/java/com/acmeflix/team7/service/MovieServiceImpl.java
+++ src/main/java/com/acmeflix/team7/service/MovieServiceImpl.java
@@ -1,5 +1,19 @@
 package com.acmeflix.team7.service;
 
-public interface MovieServiceImpl extends BaseServiceImpl<UserAccount> implements MovieService{
+import com.acmeflix.team7.domain.Movie;
+import com.acmeflix.team7.repository.MovieRepository;
 
+import java.util.List;
+
+public interface MovieServiceImpl extends BaseServiceImpl<UserAccount> implements MovieService{
+    private final MovieRepository movieRepository;
+    @Override
+    public BaseRepository<Movie, Long> getRepository() {
+        return movieRepository;
+    }
+    @Override
+    public Movie findByTitle(final String title) {
+        List<Movie> movies = movieRepository.findByTitle(title);
+        return movies.get(0);
+    }
 }
