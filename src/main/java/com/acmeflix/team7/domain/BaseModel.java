4f7f9313e80d7871074631abc6c2345d20f5eaf3
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-04 10:17:41 +0300

    Changes to BaseModel implementation.

--- src/main/java/com/acmeflix/team7/domain/BaseModel.java
+++ src/main/java/com/acmeflix/team7/domain/BaseModel.java
@@ -1,5 +1,20 @@
 package com.acmeflix.team7.domain;
 
-public abstract class BaseModel {
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@ToString
+@MappedSuperclass
+public abstract class BaseModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idGenerator")
+    @Column(updatable = false)
     private Long id;
 }
