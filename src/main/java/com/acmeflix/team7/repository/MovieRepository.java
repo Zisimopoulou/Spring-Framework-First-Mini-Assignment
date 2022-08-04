27cf8651f08971abb1c824e9b1bfa8dbb60a2a1b
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 16:21:36 +0300

    Update MovieRepository

--- src/main/java/com/acmeflix/team7/repository/MovieRepository.java
+++ src/main/java/com/acmeflix/team7/repository/MovieRepository.java
@@ -1,9 +1,10 @@
 package com.acmeflix.team7.repository;
 
 import com.acmeflix.team7.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MovieRepository extends BaseRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findByTitle(String title);
 }
