257b696d9291494d91974578fe5bb1a60ea3429d
d.paliogiannis <oldjohn0000@gmail.com>
2022-08-04 14:48:22 +0300

    dimitris

--- src/main/java/com/acmeflix/team7/domain/Profile.java
+++ src/main/java/com/acmeflix/team7/domain/Profile.java
@@ -1,11 +1,20 @@
 package com.acmeflix.team7.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+@Getter
+@Setter
+@ToString
+@Builder
 public class Profile extends BaseModel{
     private String name;
     private String country;
     private String language;
     private String gender;
     private MaturialRaingLevel maturialRaingLevel;
-
 
 }
--- src/main/java/com/acmeflix/team7/domain/UserAccount.java
+++ src/main/java/com/acmeflix/team7/domain/UserAccount.java
@@ -1,14 +1,22 @@
 package com.acmeflix.team7.domain;
 
 import com.acmeflix.team7.domain.enums.SubscriptionPlan;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.ToString;
 
 import java.util.List;
-
+@Getter
+@Setter
+@Builder
+@ToString
 public class UserAccount extends BaseModel{
     private String username;
     private String password;
     private String email;
     private List< Profile >profiles;
     private SubscriptionPlan subscriptionPlan;
+
 
 }
