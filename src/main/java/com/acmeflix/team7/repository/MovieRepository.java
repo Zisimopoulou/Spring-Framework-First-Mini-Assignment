e55a240891570f13140486f40c7e6997cb4a941d
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-04 19:16:57 +0300

    Add find recommendation and find by genre in MovieService and repository.

--- src/main/java/com/acmeflix/team7/repository/MovieRepository.java
+++ src/main/java/com/acmeflix/team7/repository/MovieRepository.java
@@ -1,10 +1,12 @@
 package com.acmeflix.team7.repository;
 
 import com.acmeflix.team7.domain.Movie;
+import com.acmeflix.team7.domain.enums.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    List<Movie> findMovieByTitle(String movieTitle);
+    Movie findMovieByTitle(String movieTitle);
+    Movie findMovieByGenre(Genre genre);
 }
--- src/main/java/com/acmeflix/team7/service/MovieServiceImpl.java
+++ src/main/java/com/acmeflix/team7/service/MovieServiceImpl.java
@@ -1,16 +1,20 @@
 package com.acmeflix.team7.service;
 
 import com.acmeflix.team7.domain.Movie;
+import com.acmeflix.team7.domain.TvShow;
+import com.acmeflix.team7.domain.enums.Country;
+import com.acmeflix.team7.domain.enums.Genre;
 import com.acmeflix.team7.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 @RequiredArgsConstructor
-public class MovieServiceImpl extends BaseServiceImpl <Movie> implements MovieService{
-
+@Slf4j
+public class MovieServiceImpl extends BaseServiceImpl<Movie> implements MovieService{
     private final MovieRepository movieRepository;
     public JpaRepository<Movie, Long> getRepository() {
         return movieRepository;
@@ -18,7 +22,21 @@ public class MovieServiceImpl extends BaseServiceImpl <Movie> implements MovieSe
 
     @Override
     public Movie findMovieByTitle(String movieTitle) {
-        List<Movie> movies = movieRepository.findMovieByTitle(movieTitle);
-        return movies.get(0);
+        return movieRepository.findMovieByTitle(movieTitle);
     }
+    @Override
+    public Movie findMovieByGenre(Genre genre) {
+        return movieRepository.findMovieByGenre(genre);
+    }
+    @Override
+    public Movie findMovieRecommendation(Movie movie,int count) {
+        log.info("Initialize movie recommendations based on the genre = {}.", movie.getGenres().get(count).getGenreName());
+        Movie movieRecommendation = findMovieByGenre(movie.getGenres().get(count));
+        log.info("Finalize movie recommendations based on the country = {}.", movie.getCountry().getCountryName());
+        if (movie.getCountry().getCountryName() == movieRecommendation.getCountry().getCountryName()){
+            return movieRecommendation;
+        }
+        return null;
+    }
+
 }
