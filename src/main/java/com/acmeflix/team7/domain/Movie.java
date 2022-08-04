42c199d0030911ad750a8a0b4b0d7f56269798ff
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 14:37:28 +0300

    Update Movie with @Annotations

--- src/main/java/com/acmeflix/team7/domain/Movie.java
+++ src/main/java/com/acmeflix/team7/domain/Movie.java
@@ -1,8 +1,15 @@
 package com.acmeflix.team7.domain;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.util.Date;
-
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie extends ContentCatalog{
     private BigDecimal duration;
     private Date date;
