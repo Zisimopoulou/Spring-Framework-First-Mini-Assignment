4be5653625515951a271d73a143e376a0400fa95
Petros Dhespollari <peterdsp@icloud.com>
2022-08-04 13:23:46 +0300

    Update Episode

--- src/main/java/com/acmeflix/team7/domain/Episode.java
+++ src/main/java/com/acmeflix/team7/domain/Episode.java
@@ -1,4 +1,19 @@
 package com.acmeflix.team7.domain;
 
-public class Episode {
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
+public class Episode extends BaseModel{
+    private String title;
+    private Date date;
+    private BigDecimal duration;
 }
