5266eae4fbd45db01c2d309c723ef51fd82fb6b9
Petros Dhespollari <peterdsp@icloud.com>
2022-08-05 16:19:55 +0300

    Update TvShowRepository

--- src/main/java/com/acmeflix/team7/repository/TvShowRepository.java
+++ src/main/java/com/acmeflix/team7/repository/TvShowRepository.java
@@ -3,6 +3,9 @@ package com.acmeflix.team7.repository;
 import com.acmeflix.team7.domain.TvShow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TvShowRepository extends JpaRepository<TvShow, Long> {
-    TvShow findTvShowByTitle(String tvShowTitle);
+    TvShow findTvShowByTitle(List<TvShow> tvShows, String tvShowTitle);
+
 }
