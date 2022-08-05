46a6a8e9eb58d60d70f1c8294653b30aa12fa2c1
Petros Dhespollari <peterdsp@icloud.com>
2022-08-05 16:21:46 +0300

    Update TvShowServiceImpl

--- src/main/java/com/acmeflix/team7/service/TvShowServiceImpl.java
+++ src/main/java/com/acmeflix/team7/service/TvShowServiceImpl.java
@@ -3,6 +3,7 @@ package com.acmeflix.team7.service;
 import com.acmeflix.team7.domain.TvShow;
 import com.acmeflix.team7.repository.TvShowRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TvShowServiceImpl extends BaseServiceImpl<TvShow> implements TvShowService{
     private final TvShowRepository tvShowRepository;
     public JpaRepository<TvShow, Long> getRepository() {
@@ -17,7 +19,13 @@ public class TvShowServiceImpl extends BaseServiceImpl<TvShow> implements TvShow
     }
 
     @Override
-    public TvShow findTvShowByTitle(String tvShowTitle) {
-        return tvShowRepository.findTvShowByTitle(tvShowTitle);
+    public TvShow findTvShowByTitle(List<TvShow> tvShows,String tvShowTitle) {
+            for (int i = 0; i < tvShows.size(); i++) {
+                if(tvShows.get(i).getTitle().equals(tvShowTitle)){
+                    return tvShows.get(i);
+                }
+            }
+            log.info("TvShow with title {} not found.", tvShowTitle);
+            return null;
     }
 }
