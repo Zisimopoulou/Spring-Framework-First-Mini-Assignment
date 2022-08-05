5d3daeb49cacffa9e127c426afd33f05b0f9b6b6
Petros Dhespollari <peterdsp@icloud.com>
2022-08-05 15:55:06 +0300

    Update MovieService

--- src/main/java/com/acmeflix/team7/service/MovieService.java
+++ src/main/java/com/acmeflix/team7/service/MovieService.java
@@ -1,16 +1,11 @@
 package com.acmeflix.team7.service;
 
 import com.acmeflix.team7.domain.Movie;
-import com.acmeflix.team7.domain.TvShow;
-import com.acmeflix.team7.domain.UserAccount;
-import com.acmeflix.team7.domain.enums.Country;
-import com.acmeflix.team7.domain.enums.Genre;
 
 import java.util.List;
 
 public interface MovieService extends BaseService<Movie> {
-    Movie findMovieByTitle(String movieTitle);
-    Movie findMovieByGenre(Genre genre);
-//    Movie findMovieByCountry(Country country);
+    Movie findMovieByTitle(List<Movie> movies,String movieTitle);
+    Movie findMovieByGenre(List<Movie> movies,String genreTitle);
     Movie findMovieRecommendation(Movie movie,int count);
 }
