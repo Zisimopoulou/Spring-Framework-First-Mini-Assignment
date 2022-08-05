312249860c48332043d96cb93249110e31d7687a
Petros Dhespollari <peterdsp@icloud.com>
2022-08-05 16:20:13 +0300

    Update TvShowService

--- src/main/java/com/acmeflix/team7/service/TvShowService.java
+++ src/main/java/com/acmeflix/team7/service/TvShowService.java
@@ -2,6 +2,8 @@ package com.acmeflix.team7.service;
 
 import com.acmeflix.team7.domain.TvShow;
 
+import java.util.List;
+
 public interface TvShowService extends BaseService<TvShow> {
-    TvShow findTvShowByTitle(String showTitle);
+    TvShow findTvShowByTitle(List<TvShow> tvShows, String tvShowTitle);
 }
