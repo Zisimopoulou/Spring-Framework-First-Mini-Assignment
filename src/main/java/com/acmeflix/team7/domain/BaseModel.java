b1e5e08d8f0f0a6277f34fac3586422e07b61732
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-06 17:00:34 +0300

    Add setter in base model.

--- src/main/java/com/acmeflix/team7/domain/BaseModel.java
+++ src/main/java/com/acmeflix/team7/domain/BaseModel.java
@@ -1,17 +1,22 @@
 package com.acmeflix.team7.domain;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
-import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
+@Setter
 @ToString
 @MappedSuperclass
 @SuperBuilder
+@Data
 public abstract class BaseModel implements Serializable {
-    private final Long id;
+    private static long serialVersionUID = 1L;
+    private Long id;
+
 }
